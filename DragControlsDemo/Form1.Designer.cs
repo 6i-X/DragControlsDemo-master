805aaf95151e49ff16b410715eee2cbc911fd44e
Bae <47786223+RustyBae@users.noreply.github.com>
2019-02-19 15:16:44 +0000

    Add files via upload

---
+++ DragControlsDemo/Form1.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace DragControlsDemo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtImage = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.MyPanel = new System.Windows.Forms.Panel();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.MyPanel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(312, 158);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(167, 55);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // txtImage
+            // 
+            this.txtImage.Location = new System.Drawing.Point(138, 330);
+            this.txtImage.Name = "txtImage";
+            this.txtImage.Size = new System.Drawing.Size(160, 31);
+            this.txtImage.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(32, 14);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 22);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "label1";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(85, 77);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(269, 30);
+            this.comboBox1.TabIndex = 3;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.SeaGreen;
+            this.panel1.Location = new System.Drawing.Point(25, 113);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(180, 100);
+            this.panel1.TabIndex = 4;
+            // 
+            // MyPanel
+            // 
+            this.MyPanel.BackColor = System.Drawing.Color.White;
+            this.MyPanel.Controls.Add(this.button1);
+            this.MyPanel.Controls.Add(this.panel1);
+            this.MyPanel.Controls.Add(this.label1);
+            this.MyPanel.Controls.Add(this.comboBox1);
+            this.MyPanel.Location = new System.Drawing.Point(12, 25);
+            this.MyPanel.Name = "MyPanel";
+            this.MyPanel.Size = new System.Drawing.Size(577, 271);
+            this.MyPanel.TabIndex = 5;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(324, 317);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(178, 55);
+            this.button2.TabIndex = 0;
+            this.button2.Text = "Save as Image";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(61, 339);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(71, 22);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Name:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.ClientSize = new System.Drawing.Size(590, 401);
+            this.Controls.Add(this.txtImage);
+            this.Controls.Add(this.MyPanel);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button2);
+            this.Font = new System.Drawing.Font("Century Gothic", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.MyPanel.ResumeLayout(false);
+            this.MyPanel.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtImage;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel MyPanel;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
